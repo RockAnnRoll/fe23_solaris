77828bcd69f2592383ca3c4b77113766c09b1aa2
RockAnnRoll <144233838+RockAnnRoll@users.noreply.github.com>
2024-02-22 12:55:15 +0100

    Torsdagens arbete

--- script.js
+++ script.js
@@ -2,11 +2,10 @@ const baseUrl=("https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com"); //Bas
 const searchField=document.getElementById("search-field");
 const submitBtn=document.getElementById("input-button");
 const resultSearch=document.getElementById("search-results");
-/*
-submitBtn.addEventListener('click', function() 
-{ */
 
 
+//Hämta api-nyckel och api-data 
+let allPlanetsData = [];
 fetch('https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com/keys', {
     method: 'POST',
 })
@@ -22,9 +21,78 @@ fetch('https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com/keys', {
 })
 .then(response => response.json())
 .then(data => {
-    console.log(data);    
+    allPlanetsData = data.bodies;   
+    console.log('allPlanetsData:', allPlanetsData); 
 })
 .catch(error => {
     console.error('Något gick fel:', error); 
 });
 
+//Hämta sökresultat och hämta data från allPlanetsData
+submitBtn.addEventListener('click', function()
+{ 
+    let newDiv = document.createElement('div');
+    newDiv.id = 'new-div';
+    newDiv.className = "overlay";
+    let footer = document.querySelector('footer');
+    document.body.insertBefore(newDiv, footer);
+    let searchText = searchField.value; // Hämtar texten från sökfältet
+
+let matchingPlanets = allPlanetsData.filter(planet => {
+    // Söker igenom planetsData och returnerar de planeter vars namn matchar söktexten
+    return planet.name.toLowerCase().includes(searchText.toLowerCase());
+});
+newDiv.innerHTML = '';
+
+matchingPlanets.forEach(planet => {
+    let planetName = document.createElement('h1');
+    planetName.textContent = planet.name;
+    planetName.className = "planetNameClass";
+
+    let latinLabel = document.createElement('h3');
+    latinLabel.innerHTML = planet.latinName;
+    latinLabel.className="planetLatinClass";
+
+ 
+    let planetInfo= document.createElement('p');
+   planetInfo.textContent = planet.desc;
+
+   let planetArea = document.createElement('p');
+planetArea.innerHTML = '<span style="font-weight:400">OMKRETS: </span>' + planet.circumference + 'km';
+
+    let distansenLabel = document.createElement('p');
+    distansenLabel.textContent = 'KM FRÅN SOLEN';
+    let distansen = document.createElement('p');
+    distansen.textContent = planet.distance + 'km';
+   distansen.style.color="yellow";
+
+  newDiv.appendChild(planetName);
+  newDiv.appendChild(latinLabel);  
+  newDiv.appendChild(planetInfo);
+  newDiv.appendChild(planetArea);
+  newDiv.appendChild(distansenLabel);
+  newDiv.appendChild(distansen);
+ 
+});
+})
+
+
+
+
+
+/*
+matchingPlanets.forEach(planet => {
+    let h1 = document.createElement('h1');
+    h1.textContent = planet.name;
+
+    let latin = document.createElement('h2');
+    latin.textContent = planet.latinName;
+
+    
+    let distansen = document.createElement('p');
+    distansen.textContent = planet.distance;
+
+    resultSearch.appendChild(h1);
+    resultSearch.appendChild(latin);
+    resultSearch.appendChild(distansen);
+   */
